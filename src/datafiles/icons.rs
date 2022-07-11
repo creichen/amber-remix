f6d23c35035e76878a4acb3a0d84f712aafcb232
Christoph Reichenbach <creichen@gmail.com>
2022-07-11 20:30:02 +0000

    Initial icons support, increased buffers to fix playback issues

---
+++ src/datafiles/icons.rs
@@ -0,0 +1,12 @@
+// Copyright (C) 2022 Christoph Reichenbach (creichen@gmail.com)
+// Licenced under the GNU General Public Licence, v3.  Please refer to the file "COPYING" for details.
+
+#[allow(unused)]
+use log::{Level, log_enabled, trace, debug, info, warn, error};
+#[allow(unused)]
+use crate::{ptrace, pdebug, pinfo, pwarn, perror};
+
+// ----------------------------------------
+// Icons are animated graphics used for map tiles.
+
+pub struct
