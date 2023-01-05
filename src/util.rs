2db9e36464d9b62c31d0cc9ae0afa88ea86ef8dc
Christoph Reichenbach <creichen@gmail.com>
2023-01-05 23:21:37 +0000

    Added documentation for debug macros, split logging/tracing

--- src/util.rs
+++ src/util.rs
@@ -1,16 +1,24 @@
-// Copyright (C) 2022 Christoph Reichenbach (creichen@gmail.com)
+// Copyright (C) 2022,23 Christoph Reichenbach (creichen@gmail.com)
 // Licenced under the GNU General Public Licence, v3.  Please refer to the file "COPYING" for details.
 
 // General utilities
 
-pub const LOGGING : bool = false;
-pub const WARNING : bool = true;
+pub const TRACING : bool = false;		// default: "false"
+pub const LOGGING : bool = TRACING || true;	// default: "true" during development (we should add a log file...)
+pub const WARNING : bool = true;		// default: "true"
 pub const CARGO_TEST : bool = cfg!(test);
 
+/// The operations listed here all fall back to println! when running unit tests.
+/// This simplifies reporting and allows us to run "past" a fatal error to spot false positives in error checking
+/// (I.e., we treat unit tests as ground truth, not the conditions around "error!()").
+
+
+/// Low-level tracing, only enabled if TRACING is set.  For performance-critical sections (e.g. audio synthesis)
+/// Typically enabled when debugging a very specific piece of functionality.
 #[macro_export]
 macro_rules! ptrace {
     ($($a:tt)*) => {
-	if crate::util::LOGGING {
+	if crate::util::TRACING {
 	    if crate::util::CARGO_TEST {
 		println!($($a)*)
 	    } else {
@@ -20,6 +28,10 @@ macro_rules! ptrace {
     }
 }
 
+/// Debugging: Detail messages that are too spammy for logging and/or intended for module maintainers,
+/// but don't affect performance to such a degree that we want to optimise them out by default.
+/// Typically enabled on a per-module basis for debugging/high-level tracing.
+/// Only enabled if LOGGING is set.
 #[macro_export]
 macro_rules! pdebug {
     ($($a:tt)*) => {
@@ -33,6 +45,10 @@ macro_rules! pdebug {
     }
 }
 
+/// Info: One-time or uncommon messages that you want users to list and ask about in issue reports.
+/// One-time messages may be lengthy (e.g., during decoding).
+/// Typically enabled for validating.
+/// Only enabled if LOGGING is set.
 #[macro_export]
 macro_rules! pinfo {
     ($($a:tt)*) => {
@@ -46,6 +62,8 @@ macro_rules! pinfo {
     }
 }
 
+/// Warning: Something is off, but we have a safe fallback.
+/// Should always be enabled (cf. WARNING)
 #[macro_export]
 macro_rules! pwarn {
     ($($a:tt)*) => {
@@ -59,6 +77,7 @@ macro_rules! pwarn {
     }
 }
 
+/// Fatal error.
 #[macro_export]
 macro_rules! perror {
     ($($a:tt)*) => {
